1f05c254920e75244ab5a4a7db79042d306c4c92
Clarence Dimitri CHARLES <ccharles@takima.fr>
2019-07-31 19:16:56 +0200

    La tondeuse doit être capable d'avancer et de pivoter lorsqu'elle reçoit plusieurs commandes

--- src/test/java/com/xebia/test/TondeuseTest.java
+++ src/test/java/com/xebia/test/TondeuseTest.java
@@ -31,7 +31,7 @@ public class TondeuseTest {
         );
     }
 
-    @DisplayName("La tondeuse doit pivoter à droite la tondeuse lorsqu'elle reçoit la commande D")
+    @DisplayName("La tondeuse doit pivoter à droite lorsqu'elle reçoit la commande D")
     @ParameterizedTest(name = "{index} - Commande {1} => actual : {0}, expected : ({2},{3},{4})")
     @MethodSource("pivoterDroiteArguments")
     void testPivoterDroiteTondeuse(Tondeuse tondeuse, char[] commande, int xExpected, int yExpected, char directionExpected){
@@ -51,7 +51,7 @@ public class TondeuseTest {
         );
     }
 
-    @DisplayName("La tondeuse doit pivoter à gauche la tondeuse lorsqu'elle reçoit la commande G")
+    @DisplayName("La tondeuse doit pivoter à gauche lorsqu'elle reçoit la commande G")
     @ParameterizedTest(name = "{index} - Commande {1} => actual : {0}, expected : ({2},{3},{4})")
     @MethodSource("pivoterGaucheArguments")
     void testPivoterGaucheTondeuse(Tondeuse tondeuse, char[] commande, int xExpected, int yExpected, char directionExpected){
@@ -69,5 +69,22 @@ public class TondeuseTest {
                 Arguments.of(new Tondeuse(0,0, 'E'), "G".toCharArray(), 0, 0, 'N'),
                 Arguments.of(new Tondeuse(0,0, 'W'), "G".toCharArray(), 0, 0, 'S')
         );
+    }
+
+    @DisplayName("La tondeuse doit être capable d'avancer et de pivoter lorsqu'elle reçoit plusieurs commandes")
+    @ParameterizedTest(name = "{index} - Commande {1} => actual : {0}, expected : ({2},{3},{4})")
+    @MethodSource("pivoterEtAvancerTondeuseArguments")
+    void testPivoterEtAvancerTondeuse(Tondeuse tondeuse, char[] commande, int xExpected, int yExpected, char directionExpected){
+        tondeuse.avancer(commande);
+
+        assertEquals(xExpected, tondeuse.getPositionTondeuse().getX());
+        assertEquals(yExpected, tondeuse.getPositionTondeuse().getY());
+        assertEquals(directionExpected, tondeuse.getPositionTondeuse().getDirection());
+    }
+
+    static Stream<Arguments> pivoterEtAvancerTondeuseArguments() {
+        return Stream.of(
+                Arguments.of(new Tondeuse(0, 0, 'N'), "AADADGAGA".toCharArray(), 2, 3, 'N')
+                );
     }
 }
