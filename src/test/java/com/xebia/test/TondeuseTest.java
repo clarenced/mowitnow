7893c3813899926630c21bf4e245ad17d2e2c50b
Clarence Dimitri CHARLES <ccharles@takima.fr>
2019-07-31 17:28:19 +0200

    Refactoring de test

--- src/test/java/com/xebia/test/TondeuseTest.java
+++ src/test/java/com/xebia/test/TondeuseTest.java
@@ -1,37 +1,32 @@
 package com.xebia.test;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TondeuseTest {
 
-    @DisplayName("La tondeuse doit avancer lorsqu'elle est en position initiale et en direction N")
-    @Test
-    void testAvancerTondeuseLorsquePositionInitialeEtDirectionNord(){
-        char[] commande = {'A'};
-
-        Tondeuse tondeuse = new Tondeuse(0,0, 'N');
+    @DisplayName("Doit avancer la tondeuse lorsqu'elle reçoit la commande A")
+    @ParameterizedTest(name = "{index} - For {1} => actual : {0}, expected : ({2},{3},{4})")
+    @MethodSource("avancerArguments")
+    void testAvancerTondeuse(Tondeuse tondeuse, char[] commande, int xExpected, int yExpected, char directionExpected){
         tondeuse.avancer(commande);
 
-        assertEquals(0, tondeuse.getX());
-        assertEquals(1, tondeuse.getY());
-        assertEquals('N', tondeuse.getDirection());
+        assertEquals(xExpected, tondeuse.getX());
+        assertEquals(yExpected, tondeuse.getY());
+        assertEquals(directionExpected, tondeuse.getDirection());
     }
 
-    @DisplayName("La tondeuse doit avancer lorsqu'elle est en position initiale et en direction S")
-    @Test
-    void testAvancerTondeuseLorsquePositionInitialeEtDirectionSud(){
-        char[] commande = {'A'};
+    static Stream<Arguments> avancerArguments(){
+        return Stream.of(
+                Arguments.of(new Tondeuse(0,0, 'N'), "A".toCharArray(),0, 1, 'N'),
+                Arguments.of(new Tondeuse(0,0, 'S'), "A".toCharArray(), 0, -1, 'S')
 
-        Tondeuse tondeuse = new Tondeuse(0,0, 'S');
-        tondeuse.avancer(commande);
-
-        assertEquals(0, tondeuse.getX());
-        assertEquals(-1, tondeuse.getY());
-        assertEquals('S', tondeuse.getDirection());
+        );
     }
-
-
 }
